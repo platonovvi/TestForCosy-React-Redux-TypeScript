6f74dda287d160829c936f14036b62224dc6bbf3
platonovvi <72863218+platonovvi@users.noreply.github.com>
2022-04-26 00:52:01 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.scss';
+import mainImage from './main-image.jpg';
+import Likes from './Likes';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="wrap">
+                    <div className="card">
+                        <div className="card-image">
+                            <img src={mainImage} alt="surfing"></img>
+                            <Likes/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ src/redux/actions.js
@@ -0,0 +1,14 @@
+import {INCREMENT, DECREMENT} from "./types";
+
+export function incrementLikes() {
+    return {
+        type: INCREMENT
+    }
+
+}
+export function decrementLikes() {
+    return {
+        type: DECREMENT
+    }
+
+}
