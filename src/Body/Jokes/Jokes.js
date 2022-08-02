b0e9fbdf78c7dc481f4c7e829c80c721072681e4
platonovvi <72863218+platonovvi@users.noreply.github.com>
2022-08-02 08:49:09 +0400

    '123'

--- src/Body/Jokes/Jokes.js
+++ src/Body/Jokes/Jokes.js
@@ -1,10 +1,6 @@
-import {useState, useEffect} from 'react'
-import {useDispatch, useSelector} from "react-redux";
-import {jokeCreate, jokesLoad} from './../../redux/actions'
 import uniqid from 'uniqid'
 
 const Jokes = ({props}) => {
-
     return (
         <div className='jokes-container'>
             {props.jokes.length && props.jokes.map(res => {
